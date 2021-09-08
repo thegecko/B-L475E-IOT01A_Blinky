2ed47ffd811af6d59d284314e355431d5e37c1be
thegecko <rob.moran@arm.com>
2021-09-08 18:58:00 +0100

    Initial Commit

---
+++ RTE/_B-L475E-IOT01A/RTE_Components.h
@@ -0,0 +1,51 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky' 
+ * Target:  'B-L475E-IOT01A' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.3 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/* Keil::CMSIS Driver:VIO:Board:B-L475E-IOT01A:1.0.0 */
+#define RTE_VIO_BOARD
+        #define RTE_VIO_B_L475E_IOT01A
+/* Keil::Device:STM32Cube Framework:STM32CubeMX:1.1.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil::Device:STM32Cube HAL:Common:1.13.0 */
+#define RTE_DEVICE_HAL_COMMON
+/* Keil::Device:STM32Cube HAL:Cortex:1.13.0 */
+#define RTE_DEVICE_HAL_CORTEX
+/* Keil::Device:STM32Cube HAL:DMA:1.13.0 */
+#define RTE_DEVICE_HAL_DMA
+/* Keil::Device:STM32Cube HAL:GPIO:1.13.0 */
+#define RTE_DEVICE_HAL_GPIO
+/* Keil::Device:STM32Cube HAL:I2C:1.13.0 */
+#define RTE_DEVICE_HAL_I2C
+/* Keil::Device:STM32Cube HAL:PWR:1.13.0 */
+#define RTE_DEVICE_HAL_PWR
+/* Keil::Device:STM32Cube HAL:RCC:1.13.0 */
+#define RTE_DEVICE_HAL_RCC
+/* Keil::Device:STM32Cube HAL:UART:1.13.0 */
+#define RTE_DEVICE_HAL_UART
+/* Keil::Device:Startup:1.3.1 */
+#define RTE_DEVICE_STARTUP_STM32L4XX    /* Device Startup for STM32L4 */
+
+
+#endif /* RTE_COMPONENTS_H */
